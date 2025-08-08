a37cd63136cfadaeeb889a7ef3aa7991b6480d0a
Chan-chan123 <domingochristian102401@gmail.com>
2025-08-08 23:22:15 +0000

    Dart simple calculator program

---
+++ Calculator.dart
@@ -0,0 +1,14 @@
+void main(){
+  int num1 = 20;
+  int num2 = 13;
+
+  int equals = num1 + num2;
+  int minus  = num1 - num2;
+  int times  = num1 * num2;
+  double divide = num1 / num2;
+
+  print("The answer is: $equals");
+  print("The answer is: $minus");
+  print("The answer is: $times");
+  print("The answer is: $divide");
+}
